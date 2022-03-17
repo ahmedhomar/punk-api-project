da25b3ae525d623ce40daf5570efa7fec01b2cd3
Ahmed Omar <ahmedomar@hey.com>
2022-03-17 18:57:50 +0000

    added some styling to beer cards

--- src/components/FiltersList/FiltersList.jsx
+++ src/components/FiltersList/FiltersList.jsx
@@ -5,15 +5,15 @@ const FiltersList = ({ abvFilter, phFilter, classicRangeFilter }) => {
     <div className="filters-section">
       <p>Filter by:</p>
       <div className="filters-list">
-        <div className="filtersList__filter">
+        <div className="filters-list__filter">
           <input onClick={abvFilter} type="checkbox" id="abv" name="abv" />
           <label htmlFor="abv">High ABV ({">"} 6.0%)</label>
         </div>
-        <div className="filtersList__filter">
+        <div className="filters-list__filter">
           <input onClick={phFilter} type="checkbox" id="ph" name="ph" />
           <label htmlFor="ph">High Acidity (pH {"<"} 4.0%)</label>
         </div>
-        <div className="filtersList__filter">
+        <div className="filters-list__filter">
           <input
             onClick={classicRangeFilter}
             type="checkbox"
